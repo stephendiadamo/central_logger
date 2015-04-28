8b8376f40fae0283a015f03567706e54eaef147a
stephendiadamo <stephen.diadamo@gmail.com>
2015-04-28 01:09:33 -0400

    - Added some server side stubs - Added an API Key wrapper

--- central_logger_server/central_logger_server.py
+++ central_logger_server/central_logger_server.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response, request, abort
+from api_key_validator import require_apikey
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.route('/logger/api/v1.0/get_error_logs', methods=['GET'])
+@require_apikey
+def get_error_logs():
+    return jsonify({})
+
+
+@app.route('/logger/api/v1.0/logs', methods=['POST'])
+@require_apikey
+def create_log():
+    return jsonify({})
+
+
+@app.route('/logger/api/v1.0/time_data', methods=['POST'])
+@require_apikey
+def time_data():
+    return jsonify({})
 
 
 if __name__ == '__main__':
-    app.run()
+    # todo: remove debug
+    app.run(port=5000, debug=True)
