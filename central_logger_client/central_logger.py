9281cd86fa64446e5c20e7f048815430ac241b87
stephendiadamo <stephen.diadamo@gmail.com>
2015-04-27 23:40:33 -0400

    Initial commit. Split project into client and server

---
+++ central_logger_client/central_logger.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, session, g, redirect, url_for, \
+    abort, render_template, flash
+
+app = Flask(__name__)
+
+app.config.update(dict(
+    USERNAME='admin',
+    PASSWORD='admin'
+))
+app.config.from_envvar('CENTRAL_LOGGER_SETTINGS', silent=True)
+
+@app.route('/')
+def landing_page():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('Logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('Logged out')
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run()
