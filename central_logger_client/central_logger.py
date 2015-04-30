270b07f49b629676919f110273b1bdf2d0221605
stephendiadamo <stephen.diadamo@gmail.com>
2015-04-30 14:16:24 -0400

    Removed useless login feature, refactored some logic, added easier access to run the heavy traffic simulation.

--- central_logger_client/central_logger.py
+++ central_logger_client/central_logger.py
@@ -1,48 +1,27 @@
-from flask import Flask, request, session, redirect, url_for, \
+from flask import Flask, request, redirect, url_for, \
     render_template, flash
 import requests
 
 app = Flask(__name__)
-
-app.config.update(dict(
-    USERNAME='admin',
-    PASSWORD='admin'
-))
-
 API_URI = 'http://localhost:5000/logger/api/v1.0/'
 
 
 @app.route('/')
 def landing_page():
-    return render_template('index.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('Logged in')
-            return redirect(url_for('add_log'))
-    return render_template('login.html', error=error)
-
-
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    return redirect(url_for('login'))
+    """
+    Return the main page
+    """
+    return render_template('add_log.html')
 
 
 @app.route('/add_log', methods=['GET', 'POST'])
 def add_log():
+    """
+    Add a single log entry.
+    """
     error = None
     if request.method == 'POST':
-        if not 'log_type' in request.form:
+        if 'log_type' not in request.form:
             error = 'Missing log type information'
         elif request.form['event_type'] is None:
             error = 'Missing event type information'
@@ -94,9 +73,9 @@ def simulate_bulk_logging():
             requests.post(API_URI + 'logs', json=data)
             successful_entries += 1
         except:
-            print 'Fail'
+            flash('Failed')
         i += 1
-    print 'Successfully added %d' % successful_entries
+    flash('Successfully added %d' % successful_entries)
     return render_template('add_log.html')
 
 
